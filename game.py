5579a21b256050964cec5d84e67385e76191ceac
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-02-17 11:19:13 +0100

    Stage 3/5: Endless game

--- game.py
+++ game.py
@@ -2,11 +2,18 @@ import random
 
 
 def get_user_option():
-    possibilities = ("scissors", "paper", "rock")
+    possibilities = ("scissors", "paper", "rock", "!exit")
+    computer_option = ""
     while True:
         user_option = input()
-        if user_option in possibilities:
-            computer_option = random.choice(possibilities)
+        try:
+            message = "Invalid input"
+            assert user_option in possibilities, message
+        except AssertionError as err:
+            print(err)
+        else:
+            if user_option != "!exit":
+                computer_option = random.choice(possibilities[:-1])
             break
     return user_option, computer_option
 
@@ -23,13 +30,17 @@ def outcome(user_option, computer_option):
 
 def main():
     user_choice, computer_choice = get_user_option()
-    battle_outcome = outcome(user_choice, computer_choice)
-    if battle_outcome == "win":
-        print(f"Well done. The computer chose {computer_choice} and failed")
-    elif battle_outcome == "lost":
-        print(f"Sorry, but the computer chose {computer_choice}")
-    elif battle_outcome == "draw":
-        print(f"There is a draw ({user_choice})")
+    while user_choice != "!exit":
+        battle_outcome = outcome(user_choice, computer_choice)
+        if battle_outcome == "win":
+            print(f"Well done. The computer chose {computer_choice} and failed")
+        elif battle_outcome == "lost":
+            print(f"Sorry, but the computer chose {computer_choice}")
+        elif battle_outcome == "draw":
+            print(f"There is a draw ({user_choice})")
+        return main()
+    else:
+        print("Bye!")
 
 
 if __name__ == "__main__":
